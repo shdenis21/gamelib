571c35b98f3c885fb749f6c9f5546f65356a6e6b
shdenis21 <deniskovalchuck@mail.ru>
2013-12-07 18:51:58 +0300

    new file:   "\321\213/AssemblyInfo.cs" 	new file:   "\321\213/Client/Client.cs" 	new file:   "\321\213/Client/ClientHandlersV1.cs" 	new file:   "\321\213/bin/Debug/gamelib.dll" 	new file:   "\321\213/common/AfterConnected.cs" 	new file:   "\321\213/common/Connection.cs" 	new file:   "\321\213/common/IAuthen.cs" 	new file:   "\321\213/common/IPacket.cs" 	new file:   "\321\213/common/InputProcessor.cs" 	new file:   "\321\213/common/OutputProccessor.cs" 	new file:   "\321\213/common/PacketBase.cs" 	new file:   "\321\213/common/PacketHandlerBase.cs" 	new file:   "\321\213/common/PacketHandlerStorage.cs" 	new file:   "\321\213/gamelib.csproj" 	new file:   "\321\213/gamelib.pidb" 	new file:   "\321\213/gamelib.sln" 	new file:   "\321\213/gamelib.userprefs" 	new file:   "\321\213/math/Vector3.cs" 	new file:   "\321\213/server/ClientContext.cs" 	new file:   "\321\213/server/Server.cs" 	new file:   "\321\213/server/ServerHandlersV1.cs"

---
+++ ы/common/InputProcessor.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+namespace gamelib.Common
+{
+	public class InputProcessor
+	{
+		public InputProcessor(NetworkStream stream, Connection connection, PacketHandlerStorage handlers)
+        {
+            this._connection = connection;
+            this._stream = stream;
+            this.Handlers = handlers;
+            Reader = new BinaryReader(this._stream);
+            this._started = false;
+        }
+
+        private NetworkStream _stream;
+        private Connection _connection; //Объект класса соединения
+        private Thread _newThread;
+        private BinaryReader Reader;
+        private bool _started;
+
+        public PacketHandlerStorage Handlers { get; set; }
+
+        private void _handlePacket()
+        {
+            int id = Reader.ReadInt32(); //Читаем id пакета
+            PacketHandlerBase handler = this.Handlers.GetHandlerById(id); //Получаем обработчик
+            handler.Reader = this.Reader;
+            handler.Read(); //Вызываем чтение
+            this._connection.Receive(handler); //Вызываем обработку
+        }
+
+        private void _worker()
+        {
+            while (!this._started)
+            {
+                _handlePacket();
+            }
+        }
+
+        public void Run()
+        {
+            this._newThread = new Thread(this._worker);
+            this._newThread.Start();
+        }
+	}
+}
+
---
+++ ы/server/ServerHandlersV1.cs
@@ -0,0 +1,17 @@
+using System;
+using gamelib.Common;
+namespace gamelib.Server
+{
+	/// <summary>
+	/// Server handlers v1.
+	/// </summary>
+	class ServerHandlersV1 : PacketHandlerStorage
+    {
+        public ServerHandlersV1()
+        {
+            //AddHandler(0, new SomePacketHandler1());
+            //AddHandler(1, new SomePacketHandler2());
+        }
+    }
+}
+
