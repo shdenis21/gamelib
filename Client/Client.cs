252470ab9f86d3803034eb292bda495b22455d02
shdenis21 <deniskovalchuck@mail.ru>
2013-12-07 19:07:45 +0300

    modified:   Client/Client.cs

--- Client/Client.cs
+++ Client/Client.cs
@@ -6,12 +6,12 @@ namespace gamelib.Client
 {
 	public class Client
 	{
-		public Client(string ip, int port, PacketHandlerStorage handlers, IAuthen auth) 
+		public Client(string ip, int port, PacketHandlerStorage handlers, IAuthen auth,AfterConnected conn) 
         {
 			bool s = false;
             this._tcpClient = new TcpClient(ip, port); 
 
-            this._connection = new Connection(this._tcpClient, handlers, auth);
+            this._connection = new Connection(this._tcpClient, handlers, auth, conn);
             this._connection.Context = this;
             this._connection.Run();
 			
